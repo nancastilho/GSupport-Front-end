a1e5d282ee1a777467c8c96ffde5d10f3d047e0a
Renan Castilho <renancalves@live.com>
2023-04-11 16:09:31 -0300

    modal

---
+++ frontend/src/components/atendimento/index.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+function Atendimento() {
+  const [cadastro, setCadastro] = useState(true);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [symptoms, setSymptoms] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Aqui você pode fazer a lógica para enviar os dados do formulário para o servidor ou para outro componente
+    console.log({ name, date, time, symptoms });
+
+    // Resetar os campos do formulário após a submissão
+    setName("");
+    setDate("");
+    setTime("");
+    setSymptoms("");
+  };
+
+  const handleCadastro = () =>{
+    setCadastro(false)
+  }
+  return (
+    <form onSubmit={handleSubmit} className="max-w-xl mx-auto">
+      <div className="mb-4">
+        <label htmlFor="name" className="block mb-1 font-medium text-gray-700">
+          Nome
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="date" className="block mb-1 font-medium text-gray-700">
+          Data
+        </label>
+        <input
+          id="date"
+          name="date"
+          type="date"
+          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="time" className="block mb-1 font-medium text-gray-700">
+          Hora
+        </label>
+        <input
+          id="time"
+          name="time"
+          type="time"
+          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="problema"
+          className="block mb-1 font-medium text-gray-700"
+        >
+          Problema
+        </label>
+        <textarea
+          id="problema"
+          name="problema"
+          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+          rows={4}
+          required
+        ></textarea>
+      </div>
+
+      <div className="mb-4">
+        <label
+          htmlFor="solucao"
+          className="block mb-1 font-medium text-gray-700"
+        >
+          Solução
+        </label>
+        <textarea
+          id="solucao"
+          name="solucao"
+          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+          rows={4}
+          required
+        ></textarea>
+      </div>
+      <div className="mt-6 text-right">
+        
+        {cadastro == true ? (
+          <button
+            type="submit"
+            className="px-4 py-2 text-white bg-blue-900 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-600"
+          >
+            Cadastrar atendimento
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
+    </form>
+  );
+}
+
+export default Atendimento;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { BsFillPlusSquareFill } from "react-icons/bs";
 import Modal from "../../components/modal";
+import Atendimento from "../../components/atendimento";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -22,23 +23,7 @@ const Home = () => {
     navigate("/");
   }
 
-  const [name, setName] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
-  const [symptoms, setSymptoms] = useState("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // Aqui você pode fazer a lógica para enviar os dados do formulário para o servidor ou para outro componente
-    console.log({ name, date, time, symptoms });
-
-    // Resetar os campos do formulário após a submissão
-    setName("");
-    setDate("");
-    setTime("");
-    setSymptoms("");
-  };
+  
   return (
     <div className="flex">
       <Navbar handleLogout={handleLogout} />
@@ -50,84 +35,7 @@ const Home = () => {
       </div>
       {isModalOpen && (
         <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-          <form onSubmit={handleSubmit} className="max-w-xl mx-auto">
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block mb-1 font-medium text-gray-700"
-              >
-                Nome
-              </label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="date"
-                className="block mb-1 font-medium text-gray-700"
-              >
-                Data
-              </label>
-              <input
-                id="date"
-                name="date"
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="time"
-                className="block mb-1 font-medium text-gray-700"
-              >
-                Hora
-              </label>
-              <input
-                id="time"
-                name="time"
-                type="time"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-                className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="symptoms"
-                className="block mb-1 font-medium text-gray-700"
-              >
-                Sintomas
-              </label>
-              <textarea
-                id="symptoms"
-                name="symptoms"
-                value={symptoms}
-                onChange={(e) => setSymptoms(e.target.value)}
-                className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-                rows={4}
-                required
-              ></textarea>
-            </div>
-            <div className="mt-6 text-right">
-              <button
-                type="submit"
-                className="px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
-              >
-                Cadastrar atendimento
-              </button>
-            </div>
-          </form>
+          <Atendimento/>
         </Modal>
       )}
     </div>

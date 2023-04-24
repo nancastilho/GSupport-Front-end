d42d291a7fb077b8d6441a521b28a6c0012f59fc
Renan Castilho <renancalves@live.com>
2023-04-24 10:53:11 -0300

    flash message

--- frontend/src/components/atendimento/CreateAtendimento.tsx
+++ frontend/src/components/atendimento/CreateAtendimento.tsx
@@ -53,16 +53,9 @@ function CreateAtendimento(props: Props) {
   const [flashMessage, setFlashMessage] = useState<FlashMessageType | null>(
     null
   );
-  const handleClose = () => {
-    setFlashMessage(null);
-  };
 
-  const handleButtonClick = () => {
-    setFlashMessage({ message: 'Mensagem de sucesso!', type: 'success' });
-  };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
 
     const formData = new FormData();
 
@@ -78,12 +71,23 @@ function CreateAtendimento(props: Props) {
         formData
       );
       console.log(response.data);
-      return <FlashMessage message={flashMessage} onClose={handleClose} />;
+
+      setFlashMessage({
+        message: "Formulário enviado com sucesso!",
+        type: "success",
+      });
       // aqui você pode implementar alguma lógica para lidar com a resposta da API
     } catch (error) {
       console.log(error);
       // aqui você pode implementar alguma lógica para lidar com o erro da API
     }
+  };
+  const handleClose = () => {
+    setFlashMessage(null);
+  };
+
+  const handleButtonClick = () => {
+    setFlashMessage({ message: "Mensagem de sucesso!", type: "success" });
   };
 
   const handleInputChange = (
@@ -98,181 +102,186 @@ function CreateAtendimento(props: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-xl mx-auto">
-      <div className="mb-4">
-        <label
-          htmlFor="NomeCliente"
-          className="block mb-1 font-medium text-gray-700"
-        >
-          Nome
-        </label>
-        <input
-          id="NomeCliente"
-          name="NomeCliente"
-          type="text"
-          value={formValues.NomeCliente}
-          onChange={handleInputChange}
-          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-          required
-        />
-      </div>
-      <div className="flex justify-between">
+    <div>
+      <form onSubmit={handleSubmit} className="max-w-xl mx-auto">
         <div className="mb-4">
           <label
-            htmlFor="dateI"
+            htmlFor="NomeCliente"
             className="block mb-1 font-medium text-gray-700"
           >
-            Data inicio
+            Nome
           </label>
           <input
-            id="dateI"
-            name="dateI"
-            type="date"
+            id="NomeCliente"
+            name="NomeCliente"
+            type="text"
+            value={formValues.NomeCliente}
             onChange={handleInputChange}
-            value={dataFormatada}
-            className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+            className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
             required
           />
         </div>
-        <div className="mb-4">
-          <label
-            htmlFor="dateF"
-            className="block mb-1 font-medium text-gray-700"
-          >
-            Data fim
-          </label>
-          <input
-            id="dateF"
-            name="dateF"
-            type="date"
-            value={dataFormatada}
-            onChange={handleInputChange}
-            className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-            required
-          />
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <div className="mb-4 ">
-          <label
-            htmlFor="time"
-            className="block mb-1 font-medium text-gray-700"
-          >
-            Hora inicio
-          </label>
-          <input
-            id="timeI"
-            name="timeI"
-            type="time"
-            value={horaFormatada}
-            onChange={handleInputChange}
-            className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="time"
-            className="block mb-1 font-medium text-gray-700"
-          >
-            Hora fim
-          </label>
-          <input
-            id="timeF"
-            name="timeF"
-            type="time"
-            value={horaFormatada}
-            onChange={handleInputChange}
-            className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-            required
-          />
-        </div>
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="Problema"
-          className="block mb-1 font-medium text-gray-700"
-        >
-          Problema
-        </label>
-        <textarea
-          id="Problema"
-          name="Problema"
-          value={formValues.Problema}
-          onChange={handleInputChange}
-          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-          rows={4}
-          required
-        ></textarea>
-      </div>
-
-      <div className="mb-4">
-        <label
-          htmlFor="Solucao"
-          className="block mb-1 font-medium text-gray-700"
-        >
-          Solução
-        </label>
-        <textarea
-          id="Solucao"
-          name="Solucao"
-          value={formValues.Solucao}
-          onChange={handleInputChange}
-          className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
-          rows={4}
-          required
-        ></textarea>
-      </div>
-      <div className="mb-6 w-full">
-        <label
-          htmlFor="cover-photo"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          Imagem
-        </label>
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center">
-            <div className="mt-4 flex text-sm leading-6 text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-              >
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  multiple
-                  className="sr-only"
-                  onChange={(e) => setImage(e.target.files)}
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs leading-5 text-gray-600">
-              PNG, JPG, GIF up to 10MB
-            </p>
+        <div className="flex justify-between">
+          <div className="mb-4">
+            <label
+              htmlFor="dateI"
+              className="block mb-1 font-medium text-gray-700"
+            >
+              Data inicio
+            </label>
+            <input
+              id="dateI"
+              name="dateI"
+              type="date"
+              onChange={handleInputChange}
+              value={dataFormatada}
+              className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="dateF"
+              className="block mb-1 font-medium text-gray-700"
+            >
+              Data fim
+            </label>
+            <input
+              id="dateF"
+              name="dateF"
+              type="date"
+              value={dataFormatada}
+              onChange={handleInputChange}
+              className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+              required
+            />
           </div>
         </div>
-        <p>Imagens selecionas</p>
-        {image ? image.length : ""}
+        <div className="flex justify-between">
+          <div className="mb-4 ">
+            <label
+              htmlFor="time"
+              className="block mb-1 font-medium text-gray-700"
+            >
+              Hora inicio
+            </label>
+            <input
+              id="timeI"
+              name="timeI"
+              type="time"
+              value={horaFormatada}
+              onChange={handleInputChange}
+              className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="time"
+              className="block mb-1 font-medium text-gray-700"
+            >
+              Hora fim
+            </label>
+            <input
+              id="timeF"
+              name="timeF"
+              type="time"
+              value={horaFormatada}
+              onChange={handleInputChange}
+              className="block w-56 px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="Problema"
+            className="block mb-1 font-medium text-gray-700"
+          >
+            Problema
+          </label>
+          <textarea
+            id="Problema"
+            name="Problema"
+            value={formValues.Problema}
+            onChange={handleInputChange}
+            className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+            rows={4}
+            required
+          ></textarea>
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="Solucao"
+            className="block mb-1 font-medium text-gray-700"
+          >
+            Solução
+          </label>
+          <textarea
+            id="Solucao"
+            name="Solucao"
+            value={formValues.Solucao}
+            onChange={handleInputChange}
+            className="block w-full px-4 py-2 leading-tight border rounded-md appearance-none focus:outline-none focus:shadow-outline-gray"
+            rows={4}
+            required
+          ></textarea>
+        </div>
+        <div className="mb-6 w-full">
+          <label
+            htmlFor="cover-photo"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Imagem
+          </label>
+          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div className="text-center">
+              <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    multiple
+                    className="sr-only"
+                    onChange={(e) => setImage(e.target.files)}
+                  />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
+              <p className="text-xs leading-5 text-gray-600">
+                PNG, JPG, GIF up to 10MB
+              </p>
+            </div>
+          </div>
+          <p>Imagens selecionas</p>
+          {image ? image.length : ""}
+        </div>
+        <div>
+          <input type="checkbox" name="plantao" id="plantao" className="mr-2" />
+          <label htmlFor="agreed" className="font-medium text-gray-700">
+            Plantão
+          </label>
+        </div>
+        <div className="mt-6 text-right">
+          <button
+            onClick={handleButtonClick}
+            type="submit"
+            className="px-4 py-2 text-white bg-blue-900 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-600"
+          >
+            Cadastrar atendimento
+          </button>
+        </div>
+      </form>
+      <div className="fixed top-0 right-0 p-4 text-white z-50">
+        <FlashMessage message={flashMessage} onClose={handleClose} />
       </div>
-      <div>
-        <input type="checkbox" name="plantao" id="plantao" className="mr-2" />
-        <label htmlFor="agreed" className="font-medium text-gray-700">
-          Plantão
-        </label>
-      </div>
-      <div className="mt-6 text-right">
-        <button
-        onClick ={handleButtonClick}
-          type="submit"
-          className="px-4 py-2 text-white bg-blue-900 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-600"
-        >
-          Cadastrar atendimento
-        </button>
-      </div>
-    </form>
+    </div>
   );
 }
 
--- frontend/src/components/flashMessage/index.tsx
+++ frontend/src/components/flashMessage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export type FlashMessageType = {
   message: string;
@@ -8,21 +8,35 @@ export type FlashMessageType = {
 const FlashMessage: React.FC<{ message: FlashMessageType | null, onClose: () => void }> = ({ message, onClose }) => {
   const [isVisible, setIsVisible] = useState<boolean>(true);
 
-  const handleCloseClick = () => {
-    setIsVisible(false);
-    onClose();
-  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+      onClose();
+    }, 5000); // Exibe a mensagem por 5 segundos
+
+    return () => clearTimeout(timer);
+  }, [message, onClose]);
 
   if (!message || !isVisible) {
     return null;
   }
 
+  const className = message.type === 'success' ? 'bg-green-500' : 'bg-red-500';
+
+  const handleCloseClick = () => {
+    setIsVisible(false);
+    onClose();
+  };
+
   return (
-    <div className={`flash-message ${message.type}`}>
-      <span>{message.message}</span>
-      <button onClick={handleCloseClick}>Fechar</button>
+    <div className="fixed top-0 right-0 p-4 text-white z-50">
+      <div className={`p-4 rounded shadow-md ${className}`}>
+        <span>{message.message}</span>
+        <button className="ml-4 text-sm font-bold" onClick={handleCloseClick}>Fechar</button>
+      </div>
     </div>
   );
+  
 };
 
 export default FlashMessage;

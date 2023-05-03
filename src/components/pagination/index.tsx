df54b02e368fd3a4a229fa63355b03e5960c772b
Renan Castilho <renancalves@live.com>
2023-05-03 10:51:43 -0300

    .

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,44 +1,85 @@
-import classnames from 'classnames';
+import React from "react";
+import classnames from "classnames";
+import { Icon } from "@iconify/react";
 
 interface PaginationProps {
-    currentPage: number;
-    totalPages: number;
-    onPageChange: (page: number) => void;
-  }
-  
-
-function renderPages(currentPage: number, totalPages: number, onPageChange: (page: number) => void) {
-  const pages = [];
-
-  for (let page = 1; page <= totalPages; page++) {
-    const isActive = currentPage === page;
-    const pageClassNames = classnames(
-      'relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium',
-      { 'bg-gray-200': isActive },
-      { 'border-gray-500': isActive },
-      { 'text-gray-500': isActive },
-      { 'hover:bg-gray-100': !isActive },
-      { 'hover:border-gray-300': !isActive },
-      { 'hover:text-gray-700': !isActive }
-    );
-
-    pages.push(
-      <li key={page}>
-        <button className={pageClassNames} onClick={() => onPageChange(page)}>
-          {page}
-        </button>
-      </li>
-    );
-  }
-
-  return pages;
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  limit?: number;
 }
-export default function Pagination({ currentPage, totalPages, onPageChange }: PaginationProps) {
-    return (
-      <nav className="relative z-0 flex shadow-sm -space-x-px" aria-label="Pagination">
-        <ul className="bg-white border border-gray-300 rounded-md divide-y divide-gray-300">
-          {renderPages(currentPage, totalPages, onPageChange)}
-        </ul>
-      </nav>
-    );
-  }
+
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  limit = 5,
+}) => {
+  
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const firstIndex = Math.max(currentPage - Math.floor(limit / 2), 0);
+  const lastIndex = Math.min(firstIndex + limit - 1, totalPages - 1);
+  const visiblePages = pages.slice(firstIndex, lastIndex + 1);
+
+  return (
+    <nav className="flex items-center justify-center" aria-label="Pagination">
+      <ul className="flex justify-center space-x-4">
+        <li>
+          <button
+            className={classnames(
+              "relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50",
+              {
+                "cursor-not-allowed": currentPage === 1,
+              }
+            )}
+            onClick={() => {
+              if (currentPage > 1) {
+                onPageChange(currentPage - 1);
+              }
+            }}
+            disabled={currentPage === 1}
+          >
+            <Icon icon={'mdi:chevron-left'}/>
+          </button>
+        </li>
+        {visiblePages.map((page) => (
+          <li key={page}>
+            <button
+              className={classnames(
+                "relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium",
+                {
+                  "bg-gray-400": currentPage === page,
+                  "hover:bg-gray-100": currentPage !== page,
+                }
+              )}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+        <li>
+          <button
+            className={classnames(
+              "relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50",
+              {
+                "cursor-not-allowed": currentPage === totalPages,
+              }
+            )}
+            onClick={() => {
+              if (currentPage < totalPages) {
+                onPageChange(currentPage + 1);
+              }
+            }}
+            disabled={currentPage === totalPages}
+          >
+            <Icon icon={'mdi:chevron-right'}/>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;

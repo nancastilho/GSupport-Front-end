cc5c4d9d42642f2efb96b704ac72c636378afbc0
Renan Castilho <renancalves@live.com>
2023-05-09 10:43:27 -0300

    identação

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,47 +1,22 @@
-
 export interface FormValues {
-    Codigo?: number | undefined;
-    Usuario?:string;
-    CodUsuario?: number;
-    CodEmpresa?: number;
-    NomeFantasia?: string;
-    NomeCliente?: string;
-    Problema?: string;
-    Solucao?: string;
-    CodSistema?: number;
-    CodMeioComunicacao?: number;
-    DataCriacao?: string;
-    DataInicio?: string;
-    DataFim?: string;
-    HoraInicio?: string;
-    HoraFim?: string;
-    Assunto?: string;
-    Imagens?: [];
-    Plantao?: number;
-  }
-
-export interface Credentials {
-    Usuario: string;
-    Senha: string;
-  }
-
-export interface LayoutProps {
-  children: React.ReactNode;
-}
-
-export interface Empresa {
-  Codigo: string;
-  NomeFantasia: string;
-}
-
-export interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-}
-
-export interface LogoutProps {
-  handleLogout: () => void;
+  Codigo?: number | undefined;
+  Usuario?: string;
+  CodUsuario?: number;
+  CodEmpresa?: number;
+  NomeFantasia?: string;
+  NomeCliente?: string;
+  Problema?: string;
+  Solucao?: string;
+  CodSistema?: number;
+  CodMeioComunicacao?: number;
+  DataCriacao?: string;
+  DataInicio?: string;
+  DataFim?: string;
+  HoraInicio?: string;
+  HoraFim?: string;
+  Assunto?: string;
+  Imagens?: [];
+  Plantao?: number;
 }
 
 export interface PaginationProps {
@@ -50,11 +25,29 @@ export interface PaginationProps {
   onPageChange: (page: number) => void;
   limit?: number;
 }
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+export interface Credentials {
+  Usuario: string;
+  Senha: string;
+}
 
+export interface Empresa {
+  Codigo: string;
+  NomeFantasia: string;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface LogoutProps {
+  handleLogout: () => void;
+}
 
 export interface OnCadastroProps {
   onCadastro: boolean;
 }
-
-
-

c982549ddaa47c30bd478eb8b8182300fa614128
Renan Castilho <renancalves@live.com>
2023-09-28 09:57:13 -0300

    .

--- src/view/search/index.tsx
+++ src/view/search/index.tsx
@@ -180,7 +180,7 @@ const SearchView = () => {
           </div>
           <div className=" h-5/6 overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full  text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+              <thead className="sticky top-0 text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                   <th scope="col" className="px-5 py-3">
                     Codigo
